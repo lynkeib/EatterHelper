8253938066e8cec0acacb2db6234b0e4487b2587
lynkeib <liuchengyin1234@126.com>
2020-02-29 20:20:30 -0800

    Users

--- EatterHelper/EatterHelper/views/Login.py
+++ EatterHelper/EatterHelper/views/Login.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from users.models import User
 
 
 class LoginView(View):
     def get(self, request):
         return render(request, "login.html")
 
-    def post(self):
-        pass
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        try:
+            user = User.objects.get(username=username)
+            if password == user.password:
+                print("password is correct")
+            else:
+                print("password if not correct")
+            print("user is", user)
+        except:
+            print("user do not exit")
+            return redirect("/")
+        return redirect("/")
--- EatterHelper/EatterHelper/views/signup.py
+++ EatterHelper/EatterHelper/views/signup.py
@@ -7,5 +7,9 @@ class Signup(View):
     def get(self, request):
         return render(request, "register.html")
 
-    def post(self):
-        pass
+    def post(self, request):
+        user = User()
+        user.username = request.POST.get("username")
+        user.password = request.POST.get("password")
+        user.save()
+        return redirect("/")
